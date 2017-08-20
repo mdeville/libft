16728dc19cae6058c7ec7b60f8a4e2ff43005830
Matthew Deville <kemiimek@live.fr>
2017-08-20 19:01:29 +0200

    Add ft_strcmp ft_strncmp

--- libft.h
+++ libft.h
@@ -21,5 +21,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
