697f3749458ecc972aa8a13038e52ee2feba86ca
Deville Matthew <kemiimek@live.fr>
2017-11-12 15:44:41 +0100

    Fix prototype ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 21:13:01 by mdeville          #+#    #+#             */
-/*   Updated: 2017/11/10 16:52:31 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/11/12 15:15:07 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ typedef struct		s_rbnode
 int					ft_memcmp(void *s1, void *s2, size_t n);
 void				*ft_memalloc(size_t size);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void				*ft_memchr(void *s, int c, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memset(void *s, int c, size_t n);
--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/16 22:48:40 by mdeville          #+#    #+#             */
-/*   Updated: 2017/08/16 23:03:34 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/11/12 15:05:30 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memchr(void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char *ptr;
 	unsigned char target;
