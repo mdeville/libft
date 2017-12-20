96228d96eb49ba59315760c54f121928561fdfdd
Deville Matthew <kemiimek@live.fr>
2017-12-20 19:51:14 +0100

    fix basename

--- libc/ft_basename.c
+++ libc/ft_basename.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 16:32:22 by mdeville          #+#    #+#             */
-/*   Updated: 2017/12/19 16:39:15 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/12/20 16:30:06 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,8 @@ char	*ft_basename(char *path)
 	i = ft_strlen(path);
 	while (i > 0 && path[i] != '/')
 		i -= 1;
-	return (path + i + 1);
+	if (path[i] != '/')
+		return (path);
+	else
+		return (path + i + 1);
 }
